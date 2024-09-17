bd63a24a173a940d2ea192c30c38053be8bbe64a
Nicola Heisch <nheisch@sensational.ch>
2024-09-17 13:27:56 +0200

    set public path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/hphb_potions_helper_app/',
   build: {
     outDir: 'docs',
   }
